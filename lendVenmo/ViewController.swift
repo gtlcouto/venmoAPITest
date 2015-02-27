6256c96a64d194ba1a0d18c5db971910b7e9cc92
Gustavo Couto <gtlcouto@Gustavos-MacBook-Pro.local>
2015-02-27 15:18:03 -0800

    Initial Commit

---
+++ lendVenmo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lendVenmo
+//
+//  Created by Gustavo Couto on 2015-02-27.
+//  Copyright (c) 2015 makr.space. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ lendVenmoTests/lendVenmoTests.swift
@@ -0,0 +1,36 @@
+//
+//  lendVenmoTests.swift
+//  lendVenmoTests
+//
+//  Created by Gustavo Couto on 2015-02-27.
+//  Copyright (c) 2015 makr.space. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class lendVenmoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
